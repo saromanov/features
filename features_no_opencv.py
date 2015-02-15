333d38853504cb63589506180721930cc00faa63
Sergey Romanov <xxsmotur@gmail.com>
2015-02-16 01:06:17 +0500

    Append

--- features_no_opencv.py
+++ features_no_opencv.py
@@ -1,7 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import ndimage
+from scipy import ndimage, misc
 
-from skimage import feature
+from skimage import feature, measure
+from skimage.filter import canny
 
 #Feature detection without using OpenCV
+
+def scipy_examples():
+	img = misc.imread('1.jpg')
+	im = ndimage.gaussian_filter(img, 5)
+	sx = ndimage.sobel(im, axis=0, mode='constant')
+	sy = ndimage.sobel(im, axis=1, mode='constant')
+	pot = np.hypot(sx,sy)
+	plt.show()
+	plt.imshow(pot)
+
+def skimage_example():
+	img = misc.imread('1.jpg')
+	im = ndimage.gaussian_filter(img, 4)
+	print(img)
+	result = canny(img)
+	contours = measure.find_contours(gimg, 0.8)
+	print(contours, ...)
